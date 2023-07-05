29379e434a8d10ab11f13703c69ef39cce5157ed
Simon Binder <oss@simonbinder.eu>
2023-07-05 19:33:28 +0200

    Add draft for query interceptor API

--- drift/lib/src/runtime/executor/executor.dart
+++ drift/lib/src/runtime/executor/executor.dart
@@ -163,3 +163,176 @@ abstract class TransactionExecutor extends QueryExecutor {
   /// a no-op.
   Future<void> rollback();
 }
+
+/// Extension to wrap a [QueryExecutor] with a [QueryInterceptor].
+extension ApplyInterceptor on QueryExecutor {
+  /// Returns a [QueryExecutor] that will use `this` executor internally, but
+  /// with calls intercepted by the given [interceptor].
+  ///
+  /// This can be used to, for instance, write a custom statement logger or to
+  /// retry failing statements automatically.
+  QueryExecutor interceptWith(QueryInterceptor interceptor) {
+    final $this = this;
+
+    if ($this is TransactionExecutor) {
+      return _InterceptedTransactionExecutor($this, interceptor);
+    } else {
+      return _InterceptedExecutor($this, interceptor);
+    }
+  }
+}
+
+/// An eunm for statement methods used by drift.
+enum RunStatementMode {
+  /// A statement without information about the kind.
+  /// See also [QueryExecutor.runCustom].
+  custom,
+
+  /// An insert statement - see [QueryExecutor.runInsert].
+  insert,
+
+  /// An update statement - see [QueryExecutor.runUpdate].
+  update,
+
+  /// A delete statement - see [QueryExecutor.delete].
+  delete
+}
+
+/// An interceptor for SQL queries.
+///
+/// This wraps an existing [QueryExecutor] implemented by drift, and by default
+/// does nothing. However, specific methods can be overridden to customize the
+/// behavior of an existing query executor.
+abstract class QueryInterceptor {
+  /// Intercept [QueryExecutor.dialect] calls.
+  SqlDialect dialect(QueryExecutor executor) => executor.dialect;
+
+  /// Intercept [QueryExecutor.beginTransaction] calls.
+  TransactionExecutor beginTransaction(QueryExecutor parent) =>
+      parent.beginTransaction();
+
+  /// Intercept [TransactionExecutor.supportsNestedTransactions] calls.
+  bool transactionCanBeNested(TransactionExecutor inner) {
+    return inner.supportsNestedTransactions;
+  }
+
+  /// Intercept [QueryExecutor.close] calls.
+  Future<void> close(QueryExecutor inner) => inner.close();
+
+  /// Intercept [TransactionExecutor.send] calls.
+  Future<void> commitTransaction(TransactionExecutor inner) {
+    return inner.send();
+  }
+
+  /// Intercept [TransactionExecutor.rollback] calls.
+  Future<void> rollbackTransaction(TransactionExecutor inner) {
+    return inner.rollback();
+  }
+
+  /// Intercept [QueryExecutor.ensureOpen] calls.
+  Future<bool> ensureOpen(QueryExecutor executor, QueryExecutorUser user) =>
+      executor.ensureOpen(user);
+
+  /// Intercept [QueryExecutor.runBatched] calls.
+  Future<void> runBatched(
+      QueryExecutor executor, BatchedStatements statements) {
+    return executor.runBatched(statements);
+  }
+
+  /// Intercept [QueryExecutor.runInsert], [QueryExecutor.runUpdate],
+  /// [QueryExecutor.runDelete] or [QueryExecutor.runCustom] calls.
+  Future<int> runSql(QueryExecutor executor, RunStatementMode mode,
+      String statement, List<Object?> args) {
+    return switch (mode) {
+      RunStatementMode.custom =>
+        executor.runCustom(statement, args).then((_) => 0),
+      RunStatementMode.insert => executor.runInsert(statement, args),
+      RunStatementMode.delete => executor.runDelete(statement, args),
+      RunStatementMode.update => executor.runUpdate(statement, args),
+    };
+  }
+
+  /// Intercept [QueryExecutor.runSelect] calls.
+  Future<List<Map<String, Object?>>> runSelect(
+      QueryExecutor executor, String statement, List<Object?> args) {
+    return executor.runSelect(statement, args);
+  }
+}
+
+class _InterceptedExecutor extends QueryExecutor {
+  final QueryExecutor _inner;
+  final QueryInterceptor _interceptor;
+
+  _InterceptedExecutor(this._inner, this._interceptor);
+
+  @override
+  TransactionExecutor beginTransaction() =>
+      _interceptor.beginTransaction(_inner);
+
+  @override
+  SqlDialect get dialect => _interceptor.dialect(_inner);
+
+  @override
+  Future<bool> ensureOpen(QueryExecutorUser user) {
+    return _interceptor.ensureOpen(_inner, user);
+  }
+
+  @override
+  Future<void> runBatched(BatchedStatements statements) {
+    return _interceptor.runBatched(_inner, statements);
+  }
+
+  @override
+  Future<void> runCustom(String statement, [List<Object?>? args]) {
+    return _interceptor.runSql(
+        _inner, RunStatementMode.custom, statement, args ?? const []);
+  }
+
+  @override
+  Future<int> runDelete(String statement, List<Object?> args) {
+    return _interceptor.runSql(
+        _inner, RunStatementMode.delete, statement, args);
+  }
+
+  @override
+  Future<int> runInsert(String statement, List<Object?> args) {
+    return _interceptor.runSql(
+        _inner, RunStatementMode.insert, statement, args);
+  }
+
+  @override
+  Future<List<Map<String, Object?>>> runSelect(
+      String statement, List<Object?> args) {
+    return _interceptor.runSelect(_inner, statement, args);
+  }
+
+  @override
+  Future<int> runUpdate(String statement, List<Object?> args) {
+    return _interceptor.runSql(
+        _inner, RunStatementMode.update, statement, args);
+  }
+
+  @override
+  Future<void> close() {
+    return _interceptor.close(_inner);
+  }
+}
+
+class _InterceptedTransactionExecutor extends _InterceptedExecutor
+    implements TransactionExecutor {
+  _InterceptedTransactionExecutor(super.inner, super.interceptor);
+
+  @override
+  Future<void> rollback() {
+    return _interceptor.rollbackTransaction(_inner as TransactionExecutor);
+  }
+
+  @override
+  Future<void> send() {
+    return _interceptor.commitTransaction(_inner as TransactionExecutor);
+  }
+
+  @override
+  bool get supportsNestedTransactions =>
+      _interceptor.transactionCanBeNested(_inner as TransactionExecutor);
+}
